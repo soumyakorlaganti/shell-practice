c0385ff2265f5a64c9adb43bdebada6c274caafd
soumya <korlagani.soumya@gmail.com>
2026-02-01 18:01:16 +0530

    actions

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -4,7 +4,7 @@ NUMBER=$1
 
 if [ $NUMBER -gt 20 ]; then
     echo "given number: $NUMBER is greater than 20"
-elif
+elif [ $NUMBER -eq 20 ]; then
     echo "given number: $NUMBER is equal to 20"
 else
     echo "given number: $NUMBER is less than 20"
