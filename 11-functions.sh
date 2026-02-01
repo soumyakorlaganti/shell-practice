727d451fec8e6d8664a7cdedb3c2faa5eb3c16f8
soumya <korlagani.soumya@gmail.com>
2026-02-01 18:55:15 +0530

    actions

---
+++ 11-functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "please run the script with root user access"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
+}
+
+dnf install nginx -y
+VALIDATE $? "installing nginx"
+
+dnf install mysql -y
+VALIDATE $? "installing mysql"
+
+dnf install nodejs -y
+VALIDATE $? "installing nodejs"
