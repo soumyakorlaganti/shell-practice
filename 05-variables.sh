451806954be08ffe00d5a5479045f370e013e667
soumya <korlagani.soumya@gmail.com>
2026-02-01 16:15:33 +0530

    actions

---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Dynamically passing variable values at the run time
+
+echo "please enter your username::"
+read USER_NAME
+
+echo "user name  is $USER_NAME"
+
+echo "please enter your password::"
+read PASSWORD
+
+echo "password is $PASSWORD"
