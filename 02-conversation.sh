ec3ee4b135b017496c607d067397aff279f1d2da
soumya <korlagani.soumya@gmail.com>
2026-02-01 16:02:43 +0530

    actions

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "raju:: Hello sai"
+echo "sai:: Hi dad"
+echo "raju:: what are you doing sai?"
+echo "sai:: I am fine dad, what about you?"
---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=raju # no space between value and =
+PERSON2=sai
+
+
+echo "$PERSON1:: Hello $PERSON2"
+echo "$PERSON2:: Hi dad"
+echo "$PERSON1:: what are you doing $PERSON2?"
+echo "$PERSON2:: I am fine dad, what about you?"
