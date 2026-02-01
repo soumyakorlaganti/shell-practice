a376132a62dba8383ceeb150d72aef3814ff05e0
soumya <korlagani.soumya@gmail.com>
2026-02-01 19:55:20 +0530

    actions

--- 12-logs.sh
+++ 12-logs.sh
@@ -5,7 +5,7 @@ LOGS_FOLDER="/var/log/shell-script"
 LOGS_FILE="/var/log/shell-script/$0.log"
 
 if [ $USERID -ne 0 ]; then
-    echo "please run the script with root user access" 
+    echo "please run the script with root user access" | tee -a $LOGS_FILE
     exit 1
 fi
 
@@ -13,10 +13,10 @@ mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "$2...FAILURE"
+        echo "$2...FAILURE" | tee -a $LOGS_FILE
         exit 1
     else
-        echo "$2...SUCCESS"
+        echo "$2...SUCCESS" | tee -a $LOGS_FILE
     fi
 }
 
