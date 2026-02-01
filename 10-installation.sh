a4ab8aeacdfbe820f851cbe10bf1a27b8691e4f4
soumya <korlagani.soumya@gmail.com>
2026-02-01 18:37:49 +0530

    actions

--- 10-installation.sh
+++ 10-installation.sh
@@ -8,4 +8,11 @@ if [ $USERID -ne 0 ]; then
 fi
 
 echo "installing nginx"
+
 dnf install nginx -y
+if [ $? -ne 0 ]
+    echo "installing nginx...FAILURE"
+    exit 1
+else
+    echo "installing nginx...SUCCESS"
+fi
