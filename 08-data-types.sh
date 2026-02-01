e891be84a616cc660b230badedf68091d85a723f
soumya <korlagani.soumya@gmail.com>
2026-02-01 17:15:32 +0530

    actions

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Addition of 2 numbers
 NUM1=100
-NUM2=soumya
+NUM2=200
 
 SUM=$(($NUM1+$NUM2))
 echo "sum is: $SUM"
+
+# Array
+FRUITS=("Apple" "Banana" "Grapes")
+
+echo "Fruits are: ${FRUITS[@]}"
+echo "First Fruit is: ${FRUITS[0]}"
+echo "Second Fruit is: ${FRUITS[1]}"
+echo "Third Fruit is: ${FRUITS[2]}"
