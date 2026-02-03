8b2765d7007903de8550a3aae166a1a8c41f1875
soumya <korlagani.soumya@gmail.com>
2026-02-03 16:56:33 +0530

    Using colors for better readability

--- 16-colors.sh
+++ 16-colors.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-
+B="\e[30m"
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+B="\e[34m"
+P="\e[35m"
+C="\e[36m"
 N="\e[0m"
 
-echo -e "$R Hello sai $N, $Y I am learning $N $G Devops $N"
+echo -e "$B Hello sai $N, $G I am learning $N $P Devops $N"
